20ace7bb577865972c08db8327a9ba98614bccbd
Edward Gomez <edward@gomez.me.uk>
2022-09-20 21:56:20 +0100

    Working site

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+from solarenergy import auth_enphase, summary
+from secret_values import enphase_client_id
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
+enphase_key = ''
+
+@app.route('/')
+def index():
+    battery, panels, exported = summary()
+    # if not panels:
+    #     return redirect(url_for('set_key'))
+    return render_template('index.html', battery=battery, panels=panels, exported=exported)
+
+@app.route('/key/', methods=('GET', 'POST'))
+def set_key():
+    global enphase_key
+    print(enphase_key)
+    EBASEURL = "https://api.enphaseenergy.com/oauth"
+    authurl = f"{EBASEURL}/authorize?response_type=code&client_id={enphase_client_id}&redirect_uri=https://www.zemogle.net"
+
+    if request.method == 'POST':
+        key = request.form['key']
+
+        if not key:
+            flash('Key is required!')
+        else:
+            enphase_key = key
+            auth_enphase(key=key)
+            return redirect(url_for('index'))
+    return render_template('form.html',link=authurl)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
--- solarenergy.py
+++ solarenergy.py
@@ -2,6 +2,11 @@ import requests
 import secret_values as secrets
 from datetime import datetime, timedelta
 from base64 import b64encode
+import json
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
 # ENPHASE_APP_KEY = os.environ['ENPHASE_APP_KEY']
 # ENPHASE_USER_ID = os.environ['ENPHASE_USER_ID']
@@ -24,9 +29,9 @@ def auth_sunsynk():
 def stats_sunsynk(headers):
     today = datetime.utcnow().strftime("%Y-%m-%d")
     r = requests.get(currentstats+today, headers=headers)
-    print(f"Battery - {r.json()['data']['soc']} %")
-    print(f"Grid Use - {r.json()['data']['gridOrMeterPower']} W")
-    return
+    logging.debug(f"Battery - {r.json()['data']['soc']} %")
+    logging.debug(f"Grid Use - {r.json()['data']['gridOrMeterPower']} W")
+    return {'battery':r.json()['data']['soc'], 'grid':r.json()['data']['gridOrMeterPower']}
 
 
 def auth_octopus():
@@ -35,11 +40,18 @@ def auth_octopus():
     baseurl = f"https://api.octopus.energy/v1/electricity-meter-points/{secrets.octopus_mpan}/meters/{secrets.octopus_serial}/consumption/"
     total = f"{baseurl}?period_from={yesterday}T00:00:00&group_by=day"
     r = requests.get(total, auth=(secrets.octopus_key,''))
-    print(f"Yesterday export - {r.json()['results'][1]['consumption']} KWh")
+    logging.debug(f"Yesterday export - {r.json()['results'][1]['consumption']} KWh")
+    return r.json()['results'][1]['consumption']
+
+def save_tokens(token, refresh_token):
+    with open('token.json','w') as fp:
+        data = {'token':token, 'refresh' : refresh_token}
+        fp.write(json.dumps(data))
     return
 
-def auth_enphase(refresh_token=None):
-    print(secrets)
+def auth_enphase(key=None, refresh_token=None):
+    if not key and not refresh_token:
+        return False
     EBASEURL = "https://api.enphaseenergy.com/oauth"
     authurl = f"{EBASEURL}/authorize?response_type=code&client_id={secrets.enphase_client_id}&redirect_uri=https://www.zemogle.net"
 
@@ -51,27 +63,54 @@ def auth_enphase(refresh_token=None):
     if refresh_token:
         url = f"{EBASEURL}/token?grant_type=refresh_token&refresh_token={refresh_token}"
     else:
-        url = f"{EBASEURL}/token?grant_type=authorization_code&redirect_uri=https://www.zemogle.net&code=Yt3O56"
+        url = f"{EBASEURL}/token?grant_type=authorization_code&redirect_uri=https://www.zemogle.net&code={key}"
     r = requests.post(url, headers=headers)
+    if not r.status_code == 200:
+        return False
     token = r.json()['access_token']
     refresh_token = r.json()['refresh_token']
-    return token, refresh_token
+    if token and refresh_token:
+        save_tokens(token, refresh_token)
+    return {'token':token, 'refresh' : refresh_token}
+
+def get_enphase_tokens():
+    with open('token.json','r') as fp:
+        data = json.loads(fp.read())
+    return data
 
 def enphase_summary(token):
     headers = {'Authorization': f'Bearer {token}'}
     summaryurl = f"https://api.enphaseenergy.com/api/v4/systems/{secrets.enphase_system_id}/summary?key={secrets.enphase_api_key}"
     r = requests.get(summaryurl, headers=headers)
-    print(f"Production today {r.json()['energy_today']/1000:.2f} KWh")
-    print(f"Current power {r.json()['current_power']/1000:.2f} KW")
+    print(summaryurl)
+    print(headers)
+    if r.status_code != 200:
+        logging.error(r.content)
+        return False
+    logging.debug(f"Production today {r.json()['energy_today']/1000:.2f} KWh")
+    logging.debug(f"Current power {r.json()['current_power']/1000:.2f} KW")
+    return {'today':r.json()['energy_today']/1000, 'current' : r.json()['current_power']/1000}
+
+def enphase_aggregate(token):
+    headers = {'Authorization': f'Bearer {token}'}
+    summaryurl = f"https://api.enphaseenergy.com/api/v4/systems/{secrets.enphase_system_id}/telemetry/production_micro?key={secrets.enphase_api_key}"
+    r = requests.get(summaryurl, headers=headers)
+    logging.debug(r.json())
     return
 
-def main():
-    token = auth_sunsynk()
-    if token:
-        headers = {'Authorization': f'Bearer {token}'}
-        stats_sunsynk(headers)
-    enphase_summary(token=secrets.enphase_token)
-    auth_octopus()
+def summary():
+    tokens = get_enphase_tokens()
+    sstoken = auth_sunsynk()
+    if sstoken:
+        headers = {'Authorization': f'Bearer {sstoken}'}
+        battery = stats_sunsynk(headers)
+    tokens = get_enphase_tokens()
+    panels = enphase_summary(token=tokens['token'])
+    if not panels:
+        tokens = auth_enphase(refresh_token=tokens['refresh'])
+        panels = enphase_summary(token=tokens['token'])
+    exported = auth_octopus()
+    return battery, panels, exported
 
-if __name__ == '__main__':
+if __name__ == '__summary__':
     main()
